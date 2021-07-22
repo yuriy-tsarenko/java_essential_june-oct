dc9126988fb245f3d0ba428219e6c31fe5715149
Dmitriy Safonov <d.lexed@gmail.com>
2021-07-22 21:01:57 +0300

    Feature 1: initial commit

---
+++ src/main/java/com/java_essential/safonov/classwork/test.java
@@ -0,0 +1,7 @@
+package com.java_essential.safonov.classwork;
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/java_essential/safonov/homework/newtest.java
@@ -0,0 +1,7 @@
+package com.java_essential.safonov.homework;
+
+public class newtest {
+    public static void main(String[] args) {
+        
+    }
+}
