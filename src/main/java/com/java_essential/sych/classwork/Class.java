cce1f56070afe6bd801b69502db6d5233f25e43e
Stepan Sych <stepansych@gmail.com>
2021-07-23 20:05:42 +0300

    FEATURE-1: initial commit

---
+++ src/main/java/com/java_essential/sych/classwork/Class.java
@@ -0,0 +1,7 @@
+package com.java_essential.sych.classwork;
+
+public class Class {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/java_essential/sych/homework/Home.java
@@ -0,0 +1,7 @@
+package com.java_essential.sych.homework;
+
+public class Home {
+    public static void main(String[] args) {
+
+    }
+}
