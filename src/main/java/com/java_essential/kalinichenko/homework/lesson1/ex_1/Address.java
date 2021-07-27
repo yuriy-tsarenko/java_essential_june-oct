7788be74db494d3ef5833e4daee2a8eccd005d22
Viktoriia <calinichenko.viktor@gmail.com>
2021-07-27 19:42:52 +0300

    FEATURE -1: homework kalinichenko lesson 1

---
+++ src/main/java/com/java_essential/kalinichenko/homework/lesson1/ex_1/Address.java
@@ -0,0 +1,59 @@
+package com.java_essential.kalinichenko.homework.lesson1.ex_1;
+
+public class Address {
+    private int index;
+    private String country;
+    private String city;
+    private String street;
+    private int house;
+    private String apartment;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+}
---
+++ src/main/java/com/java_essential/kalinichenko/homework/lesson1/ex_3/Title.java
@@ -0,0 +1,17 @@
+package com.java_essential.kalinichenko.homework.lesson1.ex_3;
+
+public class Title {
+    String booksTitle;
+
+    public String getBooksTitle() {
+        return booksTitle;
+    }
+
+    public void setBooksTitle(String booksTitle) {
+        this.booksTitle = booksTitle;
+    }
+
+    public void show() {
+        System.out.println(this.booksTitle);
+    }
+}
