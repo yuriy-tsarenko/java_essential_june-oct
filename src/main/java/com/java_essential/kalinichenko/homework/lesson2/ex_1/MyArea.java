cf5de43de8d6a7de8d881015f10031944766ff81
Viktoriia <calinichenko.viktor@gmail.com>
2021-07-30 18:17:19 +0300

    HOMEWORK -1: homework kalinichenko lesson 2 ex 1

---
+++ src/main/java/com/java_essential/kalinichenko/homework/lesson2/ex_1/MyArea.java
@@ -0,0 +1,9 @@
+package com.java_essential.kalinichenko.homework.lesson2.ex_1;
+
+public class MyArea {
+    static final float PI = 3.14F;
+
+    static float areaOgCircle(float r) {
+        return PI * r * r;
+    }
+}
