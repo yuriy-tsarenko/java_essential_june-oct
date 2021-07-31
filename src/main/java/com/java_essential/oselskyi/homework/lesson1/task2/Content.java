c864cead92bebf155ab55183a3dd352f31086c36
Oselskyi <vadimoselskyi@gmail.com>
2021-07-31 21:01:42 +0300

    HOMEWORK_OSELSKYI_LESSON1: performed lesson 1 task 1-4

---
+++ src/main/java/com/java_essential/oselskyi/homework/lesson1/task2/Content.java
@@ -0,0 +1,17 @@
+package com.java_essential.oselskyi.homework.lesson1.task2;
+
+public class Content {
+    private String content;
+
+    public void show() {
+        System.out.println("Содержание: " + content);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/java_essential/oselskyi/homework/lesson1/task2/Title.java
@@ -0,0 +1,17 @@
+package com.java_essential.oselskyi.homework.lesson1.task2;
+
+public class Title {
+    private String title;
+
+    public void show() {
+        System.out.println("Книга - " + title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
