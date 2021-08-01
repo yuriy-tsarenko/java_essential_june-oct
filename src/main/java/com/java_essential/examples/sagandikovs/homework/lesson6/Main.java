7794fbea9e1015e6f5df43e2ff4ee23520e460bf
Dmitrij <bladen1985@gmail.com>
2021-08-01 20:26:50 +0300

    Feature-1: initial commit

---
+++ src/main/java/com/java_essential/examples/sagandikovs/homework/lesson6/Main.java
@@ -0,0 +1,12 @@
+package com.java_essential.examples.sagandikovs.homework.lesson6;
+//Homework lesson 6 exercise 2
+public class Main {
+    public static void main(String[] args) {
+        Vehicle.Wheel wheel = new Vehicle().new Wheel();
+        Vehicle.Door door = new Vehicle().new Door();
+
+        wheel.print();
+        door.print();
+
+    }
+}
